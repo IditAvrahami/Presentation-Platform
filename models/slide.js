408ff95511f4f875cb7c2e500938133dd838c7fa
IditAvrahami <iditavra@gmail.com>
2024-08-08 15:40:27 +0300

    update

--- models/slide.js
+++ models/slide.js
@@ -3,19 +3,9 @@ const mongoose = require('mongoose');
 const slideSchema = new mongoose.Schema({
   title: String,
   content: String,
-  // Add other fields as needed
 });
 
 const Slide = mongoose.model('Slide', slideSchema);
 
 module.exports = Slide;
 
-
-// const mongoose = require('mongoose');
-
-// const slideSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   content: { type: String, required: true }
-// });
-
-// module.exports = mongoose.model('Slide', slideSchema);
